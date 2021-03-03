f1f7ced9967fff6150f169f1ca75016c7dd2be45
IMG Systems <anilreddy4438@hotmail.com>
2021-03-03 20:31:37 +0530

    app form

--- src/Modules/Login/login.js
+++ src/Modules/Login/login.js
@@ -4,16 +4,16 @@ import { login, loginApi } from "../../actions/login";
 import history from "../../history";
 import "../../../src/style.css";
 import GitHubLogin from "react-github-login";
-import { Button,Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password:"",
+      password: "",
       loggedin: "",
-      errors: []
-    };   
+      errors: [],
+    };
     this.onSuccessGithub = this.onSuccessGithub.bind(this);
     this.onFailure = this.onFailure.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,28 +25,47 @@ class Login extends React.Component {
         <div className="container page">
           <div className="row">
             <div className="col-md-6 offset-md-3 col-xs-12">
-            <Form noValidate  onSubmit={this.handleSubmit} >
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange=  {this.handleInputChange.bind(this,
-    'email')} />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+              <Form noValidate onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    onChange={(e) => {
+                      this.setState({ email: e.target.value });
+                    }}
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={this.state.password} onChange= {this.handleInputChange.bind(this,
-    'password')} />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" value={this.state.loggedin} onChange= {(e)=>this.handleInputChange(e)} />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={(e) => {
+                      this.setState({ password: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check
+                    type="checkbox"
+                    label="Check me out"
+                    value={this.state.loggedin}
+                    onChange={(e) => {
+                      this.setState({ loggedin: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
               <GitHubLogin
                 clientId="853e1842dfdc6900376e"
                 onSuccess={this.onSuccessGithub}
@@ -57,7 +76,6 @@ class Login extends React.Component {
                 redirectUri=""
               />
             </div>
-           
           </div>
         </div>
       </div>
@@ -69,13 +87,10 @@ class Login extends React.Component {
   onFailure(ev) {
     console.log(ev);
   }
-  handleSubmit(ev){
-    console.log(ev)
-  }
-  handleInputChange(type, ev) {
-    console.log(type,ev.target.value)
-    this.setState({ type : [ev.target.value] });
-    console.log(this.state)
+  handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    this.props.handleSubmit(this.state)
   }
 }
 const mapStateToProps = (state) => ({});
@@ -85,6 +100,9 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(loginApi(userData));
     // history.push("/user");
   },
+  handleSubmit: (userData) =>{
+    dispatch(login)
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
